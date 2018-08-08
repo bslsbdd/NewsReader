e373331e2f1cd6d39db2812babaec59ea3d80a2d
Jerry <Jerryxucy@outlook.com>
2018-08-08 19:15:57 +1000

    Initial commit

---
+++ app/src/main/java/com/example/jerry/newsreader/News.java
@@ -0,0 +1,23 @@
+package com.example.jerry.newsreader;
+
+public class News {
+
+    private String title;
+    private String content;
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setContent(String content){
+        this.content = content;
+    }
+}
